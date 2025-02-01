2d0c13577ce0942afd12ca2309a6903f842d9641
Julius Adebowale <juadebgabriel@gmail.com>
2025-02-01 13:45:47 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import AgegeBread from "./components/AgegeBread";
+import ColdMortuaryStandardStar from "./components/ColdMortuaryStandardStar";
 import Moimoi from "./components/moimoi";
 
-export default function App() {
+function App() {
   return (
     <div>
       <Moimoi />
       <AgegeBread />
+      <ColdMortuaryStandardStar />
     </div>
   );
 }
+export default App;
---
+++ src/components/ColdMortuaryStandardStar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ColdMortuaryStandardStar = () => {
+  return <div className="min-h-screen">ColdMortuaryStandardStar</div>;
+};
+
+export default ColdMortuaryStandardStar;
