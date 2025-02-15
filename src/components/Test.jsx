aefc54bc690632a8b28772d662569126bad05937
Julius Adebowale <juadebgabriel@gmail.com>
2025-02-15 13:38:00 +0100

    feat: added

---
+++ src/components/Test.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { myFunc, myNewFunc } from './utils'
+
+const Test = ({a,b}) => {
+  return (
+    <div>
+       {myNewFunc()}
+    </div>
+  )
+}
+
+export default Test
+
+
+
+
+
---
+++ src/components/utils.js
@@ -0,0 +1,7 @@
+export const myFunc = (a , b) => {
+    return result = a + b;
+}
+
+export const myNewFunc = ()=> {
+    return "I lvoe js"
+}
